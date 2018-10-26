8a8f6833eb1b221b65a2091ae383cf8431b7b049
Risabh Kumar <rahulpsd18@gmail.com>
2018-10-26 21:58:12 +0530

    Added TSLint + Git Hooks

---
+++ README.md
@@ -0,0 +1,17 @@
+A Todo app made with Express (and MongoDB)
+
+### Requirements
+
+* Node v8.11
+* MongoDB (You may make use of [mLab](https://mlab.com/) or [MongoDB Atlas](https://www.mongodb.com/cloud/atlas))
+* Editor of your choice
+* Yup, that's it!
+
+## Todo
+
+- [ ] Add and configure Mongoose
+- [ ] Design User Schema
+- [ ] Implement Auth Management
+- [ ] Implement User Management
+- [ ] Add Task Schema
+- [ ] Write Rest APIs for Basic Task CRUD
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import * as errorHandler from 'errorhandler';
 
-import app from "./app";
+import app from './app';
 
 /**
  * Error Handler. Provides full stack - remove for production
@@ -10,8 +10,8 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
-    console.log(`App is running at http://localhost:${app.get("port")} in ${app.get("env")} mode`);
+const server = app.listen(app.get('port'), () => {
+    console.log(`App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
     console.log(`Press CTRL-C to stop\n`);
 });
 
