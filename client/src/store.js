3df2f9e0bbc02e4dcaf9e278c48f2ae7833fe521
Risabh Kumar <rahulpsd18@gmail.com>
2018-10-28 22:59:10 +0530

    Added persistent storage for redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './reducers';
+import { saveState, loadState } from './localStorage';
 import promise from 'redux-promise-middleware';
+import throttle from 'lodash.throttle';
+
+const persistedStore = loadState();
 
 export const store = createStore(
     rootReducer,
+    persistedStore,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(promise())),
 );
+
+store.subscribe(throttle(() => {
+    saveState(store.getState());
+}), 1000);
